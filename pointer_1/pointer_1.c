73ed42c945a6b6da5cea3bf28dad8e7a14436e98
nayeon <83941568+nyaeon@users.noreply.github.com>
2022-06-01 18:17:42 +0900

    point ++, -- 연산

---
+++ pointer_1/pointer_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	int arr[6] = { 10,20,30,40,50,60 };
+	int* p = &arr[0];
+	int i;
+
+	for (i = 0; i < 5; i++) {
+		printf("p = %p, ", p);
+		printf("*p = %d\n", *p++);
+	}
+	printf(" *p++ = %d\n", *p);
+	printf(" *--p  = %d", *--p);
+
+	return 0;
+
+}
